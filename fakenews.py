bdbdd0327ae9b8d1abe068c809df19da7d2f78a8
Hrushertz <70201254+hrushertz@users.noreply.github.com>
2024-04-17 00:08:11 +0530

    Pre-processing Code added

--- fakenews.py
+++ fakenews.py
@@ -56,4 +56,24 @@ df.columns
 
 df.head()
 
+#prerocessing of data 
+def wordopt(text):
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub("\\W"," ",text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    return text
+
+
+#conversion of text column to string type
+df["text"] = df["text"].astype(str)
+
+
+#applying preprocessing_function(wordplot)
+df["text"] = df["text"].apply(wordopt)
+
 #tanvir
